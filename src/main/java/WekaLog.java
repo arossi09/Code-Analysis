1df0fceb2ed1f3b8535203b74ca63fa6c279ab76
blake <blakemasters31@gmail.com>
2024-07-22 12:36:56 -0700

    weka dependecy

---
+++ src/main/java/WekaLog.java
@@ -0,0 +1,18 @@
+import weka.gui.Logger;
+
+public class WekaLog implements Logger {
+
+    public WekaLog(){
+
+    }
+
+    @Override
+    public void logMessage(String s) {
+
+    }
+
+    @Override
+    public void statusMessage(String s) {
+
+    }
+}
